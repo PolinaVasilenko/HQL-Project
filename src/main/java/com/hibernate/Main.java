03df0838dd2b62889bffac9b1b01ce6487c40296
polina <polindrom@memos.tk>
2022-09-22 15:52:30 +0300

    learn and use criteriaDelete

--- src/main/java/com/hibernate/Main.java
+++ src/main/java/com/hibernate/Main.java
@@ -3,12 +3,10 @@ package com.hibernate;
 
 import com.hibernate.entity.User;
 import jakarta.persistence.Query;
-import jakarta.persistence.criteria.CriteriaBuilder;
-import jakarta.persistence.criteria.CriteriaQuery;
-import jakarta.persistence.criteria.Predicate;
-import jakarta.persistence.criteria.Root;
+import jakarta.persistence.criteria.*;
 import lombok.extern.log4j.Log4j2;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 import java.util.List;
 
@@ -19,18 +17,29 @@ public class Main {
         log.info("Hibernate tutorial");
         Session session = HibernateUtil.getSessionFactory().openSession();
 
-        CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
-        CriteriaQuery<User> criteriaQuery = criteriaBuilder.createQuery(User.class);
-        Root<User> root = criteriaQuery.from(User.class);
-        criteriaQuery.select(root);
+//        CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
+//        CriteriaQuery<User> criteriaQuery = criteriaBuilder.createQuery(User.class);
+//        Root<User> root = criteriaQuery.from(User.class);
 
-        Predicate p1 = criteriaBuilder.gt(root.get("id"), 10001);
-        Predicate p2 = criteriaBuilder.lt(root.get("id"), 20001);
+        CriteriaBuilder cb = session.getCriteriaBuilder();
+        CriteriaDelete<User> criteriaDelete = cb.createCriteriaDelete(User.class);
+        Root<User> root = criteriaDelete.from(User.class);
+        criteriaDelete.where(cb.equal(root.get("id"), 10514));
 
-        criteriaQuery.select(root).where(criteriaBuilder.and(p1, p2));
+        Transaction transaction = session.beginTransaction();
+        session.createQuery(criteriaDelete).executeUpdate();
+        transaction.commit();
 
-        Query query = session.createQuery(criteriaQuery);
-        List<User> users = query.getResultList();
+
+       // criteriaQuery.select(root);
+
+//        Predicate p1 = criteriaBuilder.gt(root.get("id"), 10001);
+//        Predicate p2 = criteriaBuilder.lt(root.get("id"), 20001);
+//
+//        criteriaQuery.select(root).where(criteriaBuilder.and(p1, p2));
+//
+//        Query query = session.createQuery(criteriaQuery);
+//        List<User> users = query.getResultList();
 
         session.close();
         HibernateUtil.close();
